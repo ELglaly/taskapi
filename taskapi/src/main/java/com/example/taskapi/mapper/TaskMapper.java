96d3287efed859e0d6f7fe948ead158eac12f678
Sherif Elglaly <sherifelglaly@gmail.com>
2025-09-17 20:34:32 +0300

    feat(mapper): implement TaskMapper with ModelMapper
    Implement TaskMapper interface and its implementation using ModelMapper for DTO and entity conversion.
    
    - Add TaskMapper interface with toDto and toEntity methods.
    - Implement TaskMapperImpl using ModelMapper for bidirectional mapping.
    - Add a constructor for dependency injection of ModelMapper.

--- taskapi/src/main/java/com/example/taskapi/mapper/TaskMapper.java
+++ taskapi/src/main/java/com/example/taskapi/mapper/TaskMapper.java
@@ -1,4 +1,13 @@
 package com.example.taskapi.mapper;
 
+import com.example.taskapi.dto.TaskDto;
+import com.example.taskapi.entity.Task;
+import com.example.taskapi.request.TaskCreateRequest;
+
 public interface TaskMapper {
+
+    TaskDto toDto(Task task);
+    Task toEntity(TaskDto taskDto);
+    Task toEntity(TaskCreateRequest taskCreateRequest);
+
 }
--- taskapi/src/main/java/com/example/taskapi/mapper/TaskMapperImpl.java
+++ taskapi/src/main/java/com/example/taskapi/mapper/TaskMapperImpl.java
@@ -1,4 +1,33 @@
 package com.example.taskapi.mapper;
 
-public class TaskMapperImpl {
+
+import com.example.taskapi.dto.TaskDto;
+import com.example.taskapi.entity.Task;
+import com.example.taskapi.request.TaskCreateRequest;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapperImpl implements TaskMapper{
+    private final ModelMapper modelMapper;
+
+    public TaskMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public TaskDto toDto(Task task) {
+        return modelMapper.map(task,TaskDto.class);
+    }
+
+    @Override
+    public Task toEntity(TaskDto taskDto) {
+        return modelMapper.map(taskDto,Task.class);
+    }
+
+    @Override
+    public Task toEntity(TaskCreateRequest taskCreateRequest) {
+        return modelMapper.map(taskCreateRequest,Task.class);
+    }
 }
